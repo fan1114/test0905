83a2250fd9e51de31aa0213b0b708a0b86f52187
fan1114 <14783781744@163.com>
2019-09-05 09:52:41 +0800

    测试idea关联Git

--- src/cn/zf/test/Demo.java
+++ src/cn/zf/test/Demo.java
@@ -2,6 +2,6 @@ package cn.zf.test;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("测试idea关联Git");
+        System.out.println("测试idea关联Git.");
     }
 }
